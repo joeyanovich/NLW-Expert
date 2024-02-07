10286419787b1f815ee0ac55a01f125c723b3f7c
joeyanovich <joedison.dias@gmail.com>
2024-02-06 23:58:15 -0300

    dependency: installed sonner

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
@@ -26,8 +27,8 @@ export function NoteCard({note}: NoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
         <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
-          <Dialog.Close>
-            
+          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+            <X className='size-5'/>
           </Dialog.Close>
           
           <div className='flex flex-1 flex-col gap-3 p-5'>
@@ -41,7 +42,7 @@ export function NoteCard({note}: NoteCardProps) {
 
           <button
             type='button'
-            className='-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+            className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
           >
             Deseja <span className='text-red-400 group-hover:underline'>apagar esta nota</span>?
           </button>
