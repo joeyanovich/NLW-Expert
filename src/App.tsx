9928c8d8580b991c8dd0b5839ab5e8f4a2978841
joeyanovich <joedison.dias@gmail.com>
2024-02-08 00:09:00 -0300

    responsive: responsive home screen notes

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export function App() {
     : notes
 
   return (
-    <div className='max-w-6xl mx-auto my-12 space-y-6'>
+    <div className='max-w-6xl mx-auto my-12 space-y-6 px-5'>
       <img src={ logo } alt="NLW Expert" />
 
       <form className='w-full'>
@@ -59,7 +59,7 @@ export function App() {
       </form>
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
