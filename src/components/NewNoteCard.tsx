8c083565edcd13763809166fdf761a87e927c286
joeyanovich <joedison.dias@gmail.com>
2024-02-08 00:02:31 -0300

    feat: method to stop recording

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -7,6 +7,8 @@ interface newNoteCardProps {
   onNoteCreated: (content: string) => void
 }
 
+let speechRecognition: SpeechRecognition | null = null
+
 export function NewNoteCard({ onNoteCreated }: newNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
   const [isRecording, setIsRecording] = useState(false)
@@ -40,14 +42,48 @@ export function NewNoteCard({ onNoteCreated }: newNoteCardProps) {
   }
 
   function handleStartRecording() {
-    setIsRecording(true)
 
-    const isSpeechRecognitionAPIAvalible = 'SpeechRecognition' is window
+    const isSpeechRecognitionAPIAvalible = 'SpeechRecognition' in window
     || 'webkitSpeechRecognition' in window
+
+    if (!isSpeechRecognitionAPIAvalible) {
+      alert('Infelizmente seu navegador não suporta a API de gravação')
+      return
+    }
+
+    setIsRecording(true)
+    setShouldShowOnboarding(false)
+
+    const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+
+    speechRecognition = new SpeechRecognitionAPI()
+
+    speechRecognition.lang = 'pt-BR'
+    speechRecognition.continuous = true
+    speechRecognition.maxAlternatives = 1
+    speechRecognition.interimResults = true
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript)
+      }, '')
+
+      setContent(transcription)
+    }
+
+    speechRecognition.onerror = (event) => {
+      console.error(event)
+    }
+
+    speechRecognition.start()
   }
 
   function handleStopRecording() {
     setIsRecording(false)
+
+    if (speechRecognition !== null) {
+      speechRecognition.stop()
+    }
   }
 
   return (
